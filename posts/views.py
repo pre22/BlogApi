75258647a37eb81c5e5b4bccf947f39f07e21579
Precious Chika <43546663+pre22@users.noreply.github.com>
2020-10-17 22:58:24 +0100

    chore: created premission.py file
    Wrote custom and project level permission for the endpoints

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,13 @@
 from rest_framework import generics
 from posts.models import Post
 from posts.serializers import PostSerializer
+from posts.permissions import IsAuthorOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
