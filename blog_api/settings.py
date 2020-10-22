cb94c1380849a383598767dcbfd63c3ddf4ce777
Precious Chika <43546663+pre22@users.noreply.github.com>
2020-10-22 10:46:29 +0100

    chore: user_auth codes
    I installed the django-allauth package using pip.
    Configured the django-allauth module
    Made a migration

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -37,12 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     # Local
     'posts',
     # 3rd-party apps
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth.registration',
 ]
 
 REST_FRAMEWORK = {
@@ -54,6 +59,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
