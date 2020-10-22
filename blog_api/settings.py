2ac48845b7340a03ed0cbd50e130a3a1c81da363
Precious Chika <43546663+pre22@users.noreply.github.com>
2020-10-22 04:59:38 +0100

    chore: Setting Token Auth

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -37,13 +37,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local
     'posts',
+    # 3rd-party apps
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
