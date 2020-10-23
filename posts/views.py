1cd218cbae855f5cd865866efa27a2bd6e73ef91
Precious Chika <43546663+pre22@users.noreply.github.com>
2020-10-23 19:57:52 +0100

    Chore: endpoint view config

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from posts.models import Post
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, UserSerializer
 from posts.permissions import IsAuthorOrReadOnly
+from django.contrib.auth import get_user_model
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -11,3 +12,11 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
